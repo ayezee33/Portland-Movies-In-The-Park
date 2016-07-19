6ad59ddb7c9cb1a09216d199d2e2c8f870f20631
Aaron Porter and Tam Nguyen <mr.tamron@gmail.com>
2016-07-18 20:04:31 -0700

    add initial google map location

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
-var mapsKey = myKey.key
+// var mapsKey = myKey.apiKey;
 
 // backend logic
-
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 45.5718618, lng: -122.6539132},
+    zoom: 16
+  });
+}
 
 // frontend logic
-$(document).load(function() {
-
-})
+$(document).ready(function() {
+  // console.log(mapsKey);
+  $("#map").append(map);
+});
