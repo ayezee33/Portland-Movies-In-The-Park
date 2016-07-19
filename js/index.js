a0b6caa6c25e1e463dc2333447a3c5f449a9c317
Aaron Porter and Tam Nguyen <mr.tamron@gmail.com>
2016-07-18 20:14:53 -0700

    add initial park marker

--- js/index.js
+++ js/index.js
@@ -1,12 +1,29 @@
 // var mapsKey = myKey.apiKey;
 
 // backend logic
+var parks = [
+  { parkName: "WoodLawn",
+    lat: 45.5718618,
+    lng: -122.6539132
+  },
+
+
+];
+var userLocation = {lat: 45.5364789, lng: -122.3940587}
+var woodLawn = {lat: 45.5718618, lng: -122.6539132}
 var map;
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 45.5718618, lng: -122.6539132},
-    zoom: 16
+    center: userLocation,
+    zoom: 10
   });
+
+  var marker = new google.maps.Marker({
+          position: woodLawn,
+          map: map,
+          title: 'WoodLawn Park'
+        });
 }
 
 // frontend logic
