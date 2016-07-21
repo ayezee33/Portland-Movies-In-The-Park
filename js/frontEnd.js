eea9be1cfabc3a9ecdf6f48f107919cf8d2357b3
April Peng and Alex Zerbach <alex.zerbach@gmail.com>
2016-07-20 20:02:37 -0700

    update js functionality for click toggle on district addresses

--- js/frontEnd.js
+++ js/frontEnd.js
@@ -1,40 +1,25 @@
 $(function() {
-  $(".southeast, .southwest, .northwest, .northeast").click(function() {
+  $("#southwest, #southeast, #northwest, #northeast").click(function() {
     $(".se, .sw, .nw, .ne").hide();
-      $(this).data('clicked', true);
-      locationCheck();
+    $(this).data('clicked', true);
 
-    });
-
-    function locationCheck() {
-      if ($(".southeast").data('clicked')) {
+      if ($("#southeast").data('clicked')) {
         $(".se").toggle();
         console.log("se");
-      } else if ($(".northeast").data('clicked')) {
+        $(this).data('clicked', false);
+      } else if ($("#northeast").data('clicked')) {
         $(".ne").toggle();
         console.log("ne");
-      } else if ($(".southwest").data('clicked')) {
+        $(this).data('clicked', false);
+      } else if ($("#southwest").data('clicked')) {
         $(".sw").toggle();
         console.log("sw");
-      } else if ($(".northwest").data('clicked')) {
+        $(this).data('clicked', false);
+      } else if ($("#northwest").data('clicked')) {
         $(".nw").toggle();
         console.log("nw");
+        $(this).data('clicked', false);
       }
-    }
 
+    });
   });
-  // $("#se").click(){
-  //   $(".sw").hide();
-  //   $(".ne").hide();
-  //   $(".nw").hide();
-  // }
-  // $("#ne").click(){
-  //   $(".se").hide();
-  //   $(".sw").hide();
-  //   $(".nw").hide();
-  // }
-  // $("#nw").click(){
-  //   $(".se").hide();
-  //   $(".ne").hide();
-  //   $(".sw").hide();
-  // }
