93a7cd2a58b5400a4b68e4a6259ff3cb3d781382
Aaron Porter <aaron.w.porter@gmail.com>
2016-07-21 21:18:57 -0700

    fix spacing and indents

--- js/frontEnd.js
+++ js/frontEnd.js
@@ -3,23 +3,22 @@ $(function() {
     $(".se, .sw, .nw, .ne, .all").hide();
     $(this).data('clicked', true);
 
-      if ($("#southeast").data('clicked')) {
-        $(".se").toggle();
-        console.log("se");
-        $(this).data('clicked', false);
-      } else if ($("#northeast").data('clicked')) {
-        $(".ne").toggle();
-        console.log("ne");
-        $(this).data('clicked', false);
-      } else if ($("#southwest").data('clicked')) {
-        $(".sw").toggle();
-        console.log("sw");
-        $(this).data('clicked', false);
-      } else if ($("#northwest").data('clicked')) {
-        $(".nw").toggle();
-        console.log("nw");
-        $(this).data('clicked', false);
-      }
-
-    });
+    if ($("#southeast").data('clicked')) {
+      $(".se").toggle();
+      console.log("se");
+      $(this).data('clicked', false);
+    } else if ($("#northeast").data('clicked')) {
+      $(".ne").toggle();
+      console.log("ne");
+      $(this).data('clicked', false);
+    } else if ($("#southwest").data('clicked')) {
+      $(".sw").toggle();
+      console.log("sw");
+      $(this).data('clicked', false);
+    } else if ($("#northwest").data('clicked')) {
+      $(".nw").toggle();
+      console.log("nw");
+      $(this).data('clicked', false);
+    }
   });
+});
